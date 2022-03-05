8df228757d42f725345ce1bf5b2579bf923ff4f2
adrien-intern <adrien.degrange@aodocs.com>
2022-03-05 09:10:49 +0100

    preprocessing statis css files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+// CSS 
+import './css/main.css'; 
+// import '@styles/assets/style.css'; 
+
+
 console.log('Hello, IT!')
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,35 @@
 const path = require('path'); 
 
 module.exports = {
-    entry: './build/index.js', 
-    output: {
-        filename: 'main.js', 
-        path: path.resolve(__dirname, 'dist'), 
+    // package & export: build -> dist 
+    entry: {
+        main: './build/index.js', 
+        // style: './src/css/main.css'
     }, 
+
+    output: {
+        path: path.resolve(__dirname, 'dist'), 
+        filename: '[name].bundle.js', 
+        clean: true, // Clean the /dist folder at first 
+    }, 
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i, 
+                use: [
+                    /* { 
+                        loader: MiniCssExtractPlugin.loader, 
+                        options: { publicPath: 'build' },  
+                    }, */ 
+                    "style-loader",  
+                    "css-loader",  
+                ], 
+            },   
+        ], 
+    }, 
+
+    plugins: [], 
 
     mode: 'development', 
 }; 
