f3711b4d4b68f3e2f2771054812d35bb4d0ec90b
adrien-intern <adrien.degrange@aodocs.com>
2022-03-12 18:07:40 +0100

    basic calls to firebase back-end

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,45 @@ const firebaseConfig = {
   measurementId: "G-LDZH7SFHNZ"
 };
 
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // const analytics = getAnalytics(app);
+
+console.log('coucou'); 
+
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'; 
+
+const auth = getAuth(); 
+
+createUserWithEmailAndPassword(auth, "blougi@boulga.com", "Blougi")
+  .then((userCredentials) => {
+    const user = userCredentials.user; 
+    console.log(user); 
+  })
+  .catch((error) => {
+    console.log(error.code + ': ' + error.message); 
+  }); 
+
+signInWithEmailAndPassword(auth, "blougi@boulga.com", "Blougi").then(
+  (userCredentials) => {
+    const user = userCredentials.user; 
+    console.log(user); 
+  }
+).catch((err) => {
+  console.log(err.code + ': ' + err.message); 
+}); 
+
+onAuthStateChanged(auth, (user) => {
+  if (user) 
+    console.log("AuthN state has changed: " + user.uid); 
+  
+  else 
+    console.log("AuthN state has changed: NULL"); 
+}); 
+
 
 
