d00bbdf30f11562e36a6eae693b2bbd26812a287
adrien-intern <adrien.degrange@aodocs.com>
2022-03-12 16:10:04 +0100

    starting auth test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +7,11 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+import { getAuth, connectAuthEmulator } from "firebase/auth"; 
+
+const auth = getAuth(); 
+connectAuthEmulator(auth, 'http://localhost:9099'); 
+
+
