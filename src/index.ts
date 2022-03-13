8a529374925b8dca772eafab195402be9789149a
adrien-intern <adrien.degrange@aodocs.com>
2022-03-13 14:21:20 +0100

    test login UI

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ import './css/main.css';
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { FirebaseApp, initializeApp } from "firebase/app";
+import firebase from 'firebase/compat/app'; 
+import { setupAuthUI } from './auth-ui'; 
+
+
 // import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -25,42 +29,11 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app: FirebaseApp = firebase.initializeApp(firebaseConfig);
+
 
 // const analytics = getAnalytics(app);
 
 console.log('coucou'); 
 
-
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'; 
-
-const auth = getAuth(); 
-
-createUserWithEmailAndPassword(auth, "blougi@boulga.com", "Blougi")
-  .then((userCredentials) => {
-    const user = userCredentials.user; 
-    console.log(user); 
-  })
-  .catch((error) => {
-    console.log(error.code + ': ' + error.message); 
-  }); 
-
-signInWithEmailAndPassword(auth, "blougi@boulga.com", "Blougi").then(
-  (userCredentials) => {
-    const user = userCredentials.user; 
-    console.log(user); 
-  }
-).catch((err) => {
-  console.log(err.code + ': ' + err.message); 
-}); 
-
-onAuthStateChanged(auth, (user) => {
-  if (user) 
-    console.log("AuthN state has changed: " + user.uid); 
-  
-  else 
-    console.log("AuthN state has changed: NULL"); 
-}); 
-
-
-
+setupAuthUI(); 
